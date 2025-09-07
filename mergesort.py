6836077ff62c2378c63b588a6b848ced7bf82db7
Randitya123 <82374360+Randitya123@users.noreply.github.com>
2025-09-07 09:27:56 +0200

    Update mergesort.py

--- mergesort.py
+++ mergesort.py
@@ -5,13 +5,35 @@ for i in range(size):
     l1.append(user)
 print(l1)
 size2=1
-while size>1:
+while size2<size:
     l2=[]
-    for i in range(0,size,2):
-        if i+1<size:
-            leftstart=l1[i]
-            rightstart=l1[i+1]
+    for i in range(0,size,2*size2):
+        #if i+1<size:
+            leftstart=l1[i:i+size2]
+            rightstart=l1[i+size2:i+2*size2]
             l3=[]
             a,b=0,0
             #merging
-            while a<len[leftstart] and b<len[rightstart]
+            while a<len(leftstart)and b<len(rightstart):
+                if leftstart[a]<rightstart[b]:
+                    l3.append(leftstart[a])
+                    a+=1
+                else:
+                    l3.append(rightstart[b])
+                    b+=1
+            #add remainign elemant
+            while a<len(leftstart):
+                l3.append(leftstart[a])
+                a+=1
+            while b<len(rightstart):
+                l3.append(rightstart[b])
+                b+=1
+            l2.extend(l3)
+    l1=l2
+    size2*=2
+print(l1)
+
+
+                
+
+

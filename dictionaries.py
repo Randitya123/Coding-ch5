d571208187dc7586da92bc9088c6fa609252299f
Randitya123 <82374360+Randitya123@users.noreply.github.com>
2025-10-06 18:16:14 +0200

    Update dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -1,6 +1,21 @@
-dict={"Name":"Randitya","Age":14,"Gender":"Male"}
-#print(dict)
-#print(dict["Gender"])
+dict={"Name":"Randitya","Age":14,"Gender":"Male","Gender":"F"}
+print(dict)
+   
 #len of dictionary
-len(dict)
 print(len(dict))
+#adding a new value
+dict["new"]="yes"
+print(dict)
+dict["Name"]="yes"
+print(dict)
+#removing a new value
+del dict["Name"]
+print(dict)
+
+dict.pop("Age")
+print(dict)
+
+#some extra methods
+print(dict.keys())
+print(dict.values())
+print(dict.items())
